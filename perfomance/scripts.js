1f2579c6112f9e92f1394a64f3c2cb282692aeac
Nexis Sis <nexis.sis@gmail.com>
2018-10-24 17:47:39 +0300

    removed unused js

--- perfomance/scripts.js
+++ perfomance/scripts.js
@@ -232,14 +232,8 @@ arrowLeftScens.addEventListener('click', function () {
   }
 });
 
-const selectButton = document.querySelector('.filter__select-button');
-const selectButtonText = document.querySelector('.filter__select-button .button__text');
 const selectOptions = document.querySelectorAll('.filter__select-item');
 const popup = document.querySelector('.filter__select-popup');
-
-selectButton.addEventListener('click', function() {
-  popup.classList.toggle('filter__select-popup_open');
-});
 
 let widths = '';
 window.addEventListener('scroll', function() {
@@ -254,7 +248,6 @@ selectOptions.forEach(o => {
     selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
     e.target.classList.toggle('filter__select-item_checked', true);
     popup.classList.toggle('filter__select-popup_open', false);
-    selectButtonText.innerText = e.target.innerText;
   })
 });
 
