b38505f2b3a99054c9072bcbc8f0cf2565b1494e
Nexis Sis <nexis.sis@gmail.com>
2018-11-19 22:20:26 +0300

    safari mobile fix

--- sendmail.php
+++ sendmail.php
@@ -1,15 +1,21 @@
 <?php
 try {
-    if($_POST){
-        $name = $_POST['popup__name'];
-        $email = $_POST['popup__email'];
-        $tel = $_POST['popup__tel'];
-        $message = 'Имя: ' . $name . ' Email: ' . $email . ' Телефон: ' . $tel . ' Сообщение: ' . $_POST['form_msg'];
-
-        $headers = "MIME-Version: 1.0" . "\r\n";
+    if(isset($_POST['submit'])){
+         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
         $headers .= 'From: YOUX WEBSITE. "\r\n"';
 
+
+
+        if(!empty($_POST['popup__tel'])){
+           $tel = $_POST['popup__tel'];
+        }else{
+            throw new Exception('Tel not found', 403);
+        }
+        $name = $_POST['popup__name'];
+        $email = $_POST['popup__email'];
+
+        $message = 'Имя: ' . $name . ' Email: ' . $email . ' Телефон: ' . $tel . ' Сообщение: ' . $_POST['form_msg'];
 
         //send email
         if(mail("nexis.sis@gmail.com", "YOUX WEBSITE. Письмо от : " .$email, $message,$headers)){
