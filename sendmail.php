cd3e37f69515bde486e7e674a3986b0db90829a3
Nexis Sis <nexis.sis@gmail.com>
2018-11-19 23:29:51 +0300

    update

--- sendmail.php
+++ sendmail.php
@@ -1,21 +1,19 @@
 <?php
 try {
     if(isset($_POST['submit'])){
-         $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
         $headers .= 'From: YOUX WEBSITE. "\r\n"';
 
+//        if(empty($_POST['popup__tel'] || empty($_POST['popup__tel'])){
+//            throw new Exception('Tel not found', 403);
+//        }
 
-
-        if(!empty($_POST['popup__tel'])){
-           $tel = $_POST['popup__tel'];
-        }else{
-            throw new Exception('Tel not found', 403);
-        }
         $name = $_POST['popup__name'];
+        $tel = $_POST['popup__tel'];
         $email = $_POST['popup__email'];
 
-        $message = 'Имя: ' . $name . ' Email: ' . $email . ' Телефон: ' . $tel . ' Сообщение: ' . $_POST['form_msg'];
+        $message = 'Имя: ' . $name . ' Email: ' . $email . ' Телефон: ' . $tel . ' Сообщение: ' . $_POST['popup__text'];
 
         //send email
         if(mail("nexis.sis@gmail.com", "YOUX WEBSITE. Письмо от : " .$email, $message,$headers)){
@@ -24,7 +22,7 @@ try {
                 ));
         }else{
             throw new Exception('Email not sent', 500);
-         }
+        }
      }
 } catch (Exception $e) {
      echo json_encode(array(
